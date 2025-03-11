b61477da6174d1e87a50e3bab2c75c5ee05787bb
GBATZOLIS <gb511@cam.ac.uk>
2025-03-11 15:12:23 +0200

    fix device issues

--- configs/sphere.py
+++ configs/sphere.py
@@ -12,7 +12,7 @@ def get_config():
 
     # Training settings
     config.training = training = ml_collections.ConfigDict()
-    training.device = "cpu"  # change to "cuda" if available
+    training.device = "cuda:0"  # change to "cuda" if available
     training.gpus = 1  # Number of GPUs to use
     training.epochs = 1000
     training.checkpoint_frequency = 20
